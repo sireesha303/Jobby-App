424234f8861f3ae006b65d10e8eb1d415d2b88ed
sireesha <msireesha30031999@gmail.com>
2022-06-17 23:00:58 +0530

    storing username and passowrd fields from input field to state

--- src/components/Login/index.js
+++ src/components/Login/index.js
@@ -1,12 +1,32 @@
+import {useState} from 'react'
+
 import './index.css'
 
 const Login = () =>{
+    const [username,setUsername] = useState()
+    const [password,setPassword] = useState()
+
+    const onFormSubmit = event => {
+        event.preventDefault();
+        // console.log("testing form submit")
+    }
+
+    const onChangeOfUsername = event => {
+        // console.log(event.target.value)
+        setUsername(event.target.value)
+    }
+
+    const onChangeOfPassword = event =>{
+        // console.log(event.target.value)
+        setPassword(event.target.value)
+    }
 
     const renderUsernameField = () =>{
+        
         return(
             <div className='input-container'>
-            <lable htmlFor="username" className="input-label">USERNAME</lable>
-            <input type="text" id="username" className='input-el'/>
+            <label htmlFor="username" className="input-label">USERNAME</label>
+            <input type="text" id="username" className='input-el' onChange={onChangeOfUsername} value={username}/>
             </div>
         )
        
@@ -15,23 +35,21 @@ const Login = () =>{
     const renderPasswordField = () =>{
         return(
             <div className='input-container'>
-            <lable htmlFor="password" className="input-label">PASSWORD</lable>
-            <input type="text" id="passwprd" className='input-el'/>
+            <label htmlFor="password" className="input-label">PASSWORD</label>
+            <input type="password" id="passwprd" className='input-el' onChange={onChangeOfPassword} value={password}/>
             </div>
         )
     }
+
     return(
         <div className="app-container">
-            <form className="form-container">
+            <form className="form-container" onSubmit={onFormSubmit}>
                 <img src="https://assets.ccbp.in/frontend/react-js/logo-img.png" alt="jobby-webstire-logo" className='logo-img'/>
                 <div className='username-password-container'>
                     {renderUsernameField()}
                     {renderPasswordField()}
-
                 </div>
-                <button type="submit" className='login-btn'>Login</button>
-                    
-        
+                <button type="submit" className='login-btn'>Login</button>       
             </form>
         </div>
        
