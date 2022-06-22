aa1a3c51f2cc74cc01e3462579a2582a215b0717
sireesha <msireesha30031999@gmail.com>
2022-06-22 14:55:09 +0530

    adding responsive changes done to jobs and jobdetails routes

--- src/components/Jobs/index.js
+++ src/components/Jobs/index.js
@@ -65,47 +65,51 @@ const Jobs = () =>{
         <>
             <Header />
             <div className="jobs-bg-container" id="jobs-bg-container">
-            <div className="profile-filters-container">
-                <div className="profile-container">
-                    <img src={`${userInfo.profileImgUrl}`} alt="Profile-Img-Url" className="profile-img"/>
-                    <h1 className="name-el">{userInfo.name}</h1>
-                    <p className="short-bio">{userInfo.shortBio}</p>
+                <div className="search-container-for-sm">
+                        <input type="search" className="search-el" placeholder="Search" />
+                        <div className="search-icon-container">
+                            <AiOutlineSearch fill="white"/>
+                        </div>
                 </div>
-                <hr className="line-break"/>
-                <h1 className="filter-heading">Type Of Employement</h1>
-                <input type="checkbox" id="fulltime" name="fav_language" value="Full Time"/>
-                <label htmlFor="fulltime" className="filter-label">Full Time</label><br/>
-                <input type="checkbox" id="parttime" name="fav_language" value="Part Time"/>
-                <label htmlFor="parttime" className="filter-label">Part Time</label><br/>
-                <input type="checkbox" id="freelance" name="fav_language" value="Freelance"/>
-                <label htmlFor="FreeLance" className="filter-label">Freelance</label><br/>
-                <input type="checkbox" id="internship" name="fav_language" value="Internship"/>
-                <label htmlFor="internship" className="filter-label">Internship</label><br/>
-                <hr className="line-break"/>
-                <h1 className="filter-heading">Salary Range</h1>
-                <input type="radio" id="10lpa" name="fav_language" value="10LPA"/>
-                <label htmlFor="10lpa" className="filter-label">10LPA and above</label><br/>
-                <input type="radio" id="20lpa" name="fav_language" value="20LPA"/>
-                <label htmlFor="20lpa" className="filter-label">20LPA and above</label><br/>
-                <input type="radio" id="10lpa" name="fav_language" value="30LPA"/>
-                <label htmlFor="30lpa" className="filter-label">30LPA and above</label><br/>
-                <input type="radio" id="10lpa" name="fav_language" value="40LPA"/>
-                <label htmlFor="40lpa" className="filter-label">40LPA and above</label><br/>
-            </div>
-            <div>
-                <div className="search-container">
-                    <input type="search" className="search-el" placeholder="Search" />
-                    <div className="search-icon-container">
-                        <AiOutlineSearch fill="white"/>
+                <div className="profile-filters-container">
+                    <div className="profile-container">
+                        <img src={`${userInfo.profileImgUrl}`} alt="Profile-Img-Url" className="profile-img"/>
+                        <h1 className="name-el">{userInfo.name}</h1>
+                        <p className="short-bio">{userInfo.shortBio}</p>
                     </div>
+                    <hr className="line-break"/>
+                    <h1 className="filter-heading">Type Of Employement</h1>
+                    <input type="checkbox" id="fulltime" name="fav_language" value="Full Time"/>
+                    <label htmlFor="fulltime" className="filter-label">Full Time</label><br/>
+                    <input type="checkbox" id="parttime" name="fav_language" value="Part Time"/>
+                    <label htmlFor="parttime" className="filter-label">Part Time</label><br/>
+                    <input type="checkbox" id="freelance" name="fav_language" value="Freelance"/>
+                    <label htmlFor="FreeLance" className="filter-label">Freelance</label><br/>
+                    <input type="checkbox" id="internship" name="fav_language" value="Internship"/>
+                    <label htmlFor="internship" className="filter-label">Internship</label><br/>
+                    <hr className="line-break"/>
+                    <h1 className="filter-heading">Salary Range</h1>
+                    <input type="radio" id="10lpa" name="fav_language" value="10LPA"/>
+                    <label htmlFor="10lpa" className="filter-label">10LPA and above</label><br/>
+                    <input type="radio" id="20lpa" name="fav_language" value="20LPA"/>
+                    <label htmlFor="20lpa" className="filter-label">20LPA and above</label><br/>
+                    <input type="radio" id="10lpa" name="fav_language" value="30LPA"/>
+                    <label htmlFor="30lpa" className="filter-label">30LPA and above</label><br/>
+                    <input type="radio" id="10lpa" name="fav_language" value="40LPA"/>
+                    <label htmlFor="40lpa" className="filter-label">40LPA and above</label><br/>
                 </div>
-                
-                <ul>
-                    {jobslist.map((eachJob)=><JobDetailsItem jobDetails={eachJob} />)}
-                </ul>
+                <div>
+                    <div className="search-container">
+                        <input type="search" className="search-el" placeholder="Search" />
+                        <div className="search-icon-container">
+                            <AiOutlineSearch fill="white"/>
+                        </div>
+                    </div>
+                    <ul>
+                        {jobslist.map((eachJob)=><JobDetailsItem jobDetails={eachJob} />)}
+                    </ul>
+                </div>
             </div>
-
-        </div>
         </>
       
         
