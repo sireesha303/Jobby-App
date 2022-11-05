cbe0d586d85240a43c324c7d910a5d7a421c02f3
sireesha <msireesha30031999@gmail.com>
2022-11-05 11:48:11 +0530

    adding notfound page

--- src/components/NotFound/index.js
+++ src/components/NotFound/index.js
@@ -1,8 +1,24 @@
+import Header from '../Header';
+import './index.css';
 
-const NotFound = () =>{
+const NotFound = (props) =>{
+    const {history} = props
+
+    const onClickOfGoHome = () =>{
+        history.replace("/");
+    }
 
     return(
-        <h1>NotFound Page</h1>
+        <div className='not-found-contianer'>
+            <Header />
+            <div className='not-found-info-container'>
+                <h1 className='text-danger'>404 - Page Not Found</h1>
+                <p className='error-info'>Uh oh, we can't seem to find the page you're looking for.Try going back or to Home.</p>
+                    
+                <button className='go-home-btn' onClick={onClickOfGoHome}>Go Home</button>
+            </div>
+        </div>
+       
     )
 }
 
